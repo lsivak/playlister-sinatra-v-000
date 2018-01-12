a613515ce6f91a99031fe1ffc5eed69a96d56190
Lara Sanders <larasanders@gmail.com>
2018-01-12 20:25:21 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
     has_many :genres, through: :songs
 
     def slug
-      Artist.name.downcase.gsub(" ","-")
+      name.downcase.gsub(" ","-")
 end
     def self.find_by_slug_method(slug)
     Artist.all.find{|artist| artist.slug == slug}
